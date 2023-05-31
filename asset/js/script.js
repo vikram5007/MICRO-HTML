61733fc78534a517e06064ccccbd9f235d84c7b3
vikram5007 <raghulkalai92@gmail.com>
2023-05-31 17:03:02 +0530

    tuesday final commit

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,10 +1,48 @@
-var name_val=document.querySelector("#user");
-var frame=document.getElementById("framer");
-name_val.addEventListener("click",nameDisp);
+var parentElement = document.getElementById('pageholder');
 
-function nameDisp(){
+var name_val = document.querySelector("#user");
+name_val.addEventListener("click", nameDisp);
 
-    frame.src="/asset/components/vikram/index.html";
-    
+var task = document.getElementById("task");
+task.addEventListener("click", taskadder);
+
+
+var name_flag = "hidden";
+var task_flag = "hidden";
+
+var NameElement = document.createElement('iframe');
+var TaskElement = document.createElement('iframe');
+
+NameElement.style.width="100%";
+TaskElement.style.width="100%"; 
+
+function nameDisp() {
+    if (name_flag != "hidden") {
+        name_flag = "hidden";
+        parentElement.removeChild(NameElement);
+        console.log(name_flag)
+    } else {
+        name_flag = "show";
+        NameElement.src = "/asset/components/vikram/index.html";
+        parentElement.appendChild(NameElement);
+        console.log(name_flag)
+    }
+}
+
+function taskadder() {
+    // Create the new element
+    if (task_flag != "hidden") {
+        task_flag = "hidden"
+        parentElement.removeChild(TaskElement);
+        return;
+    } else {
+        task_flag = "show";
+        TaskElement.src = "/asset/components/assigned_task/index.html";
+        // Get the reference to the parent element of the previously created element
+        // Insert the new element as the last child of the parent element
+        parentElement.appendChild(TaskElement);
+
+    }
+
 }
 
